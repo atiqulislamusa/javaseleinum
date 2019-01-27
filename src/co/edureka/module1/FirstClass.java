d969f72c6987f7595cd1015d5daf96d283c59f6d
atiqulislamusa <38233605+atiqulislamusa@users.noreply.github.com>
2019-01-27 12:17:55 -0500

    27 Jan Change1

---
+++ src/co/edureka/module1/FirstClass.java
@@ -0,0 +1,9 @@
+package co.edureka.module1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Edurake Selenium Course by Abhresh");
+	}
+
+}
